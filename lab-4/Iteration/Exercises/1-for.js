3d349cb470c42bfa9341f6919d8af8ec0e03fc57
llliinaaa08-create <llliinaa.a08@gmail.com>
2025-11-01 22:38:06 +0200

    Delete lab-4 directory

--- lab-4/Iteration/Exercises/1-for.js
+++
@@ -1,9 +0,0 @@
-function sumFor(...args) {
-    let total = 0;
-    for (let i = 0; i < args.length; i++) {
-        total += args[i];
-    }
-    return total;
-}
-const a = sumFor(1, 2, 3);
-console.log(a)
--- lab-4/Iteration/Exercises/2-for-of.js
+++
@@ -1,9 +0,0 @@
-function sumForOf(...args) {
-    let total = 0;
-    for (let num of args) {
-        total += num;
-    }
-    return total;
-}
-const b = sumForOf(0);   
-console.log(b)
